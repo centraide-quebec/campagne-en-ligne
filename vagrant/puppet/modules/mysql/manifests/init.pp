6c0cddfd3fecec1828e74c59f2f33b2b1cde9b7d
bchhun <bernard.chhun@gmail.com>
2013-08-20 00:39:51 -0400

    new mysql class to make it possible to create a database

--- vagrant/puppet/modules/mysql/manifests/init.pp
+++ vagrant/puppet/modules/mysql/manifests/init.pp
@@ -1,13 +1,72 @@
-class mysql::install {
-  $password = "vagrant"
-  package { "mysql-client": ensure => installed }
-  package { "mysql-server": ensure => installed }
+## ENGLOBE MYSQL
+class mysql {
+    include mysql::install, mysql::service
+}
 
-  exec { "Set MySQL server root password":
-    subscribe => [ Package["mysql-server"], Package["mysql-client"] ],
-    refreshonly => true,
-    unless => "mysqladmin -uroot -p$password status",
-    path => "/bin:/usr/bin",
-    command => "mysqladmin -uroot password $password",
+class mysql::params {
+  $data_path = "/var/lib/mysql"
+}
+
+## INSTALL MYSQL
+class mysql::install {
+    include mysql::params
+
+    package { "mysql-server": ensure => installed }
+    package { "mysql-client": ensure => installed }
+}
+
+## RUN MYSQL
+class mysql::service {
+    include mysql::params
+
+  service { "mysql":
+    enable => true,
+    ensure => running,
+    hasstatus => true,
+    require => Package["mysql-server"],
   }
+
+  #file { "/var/lib/mysql/my.cnf":
+  # owner => "mysql", group => "mysql",
+  #       source => "puppet:///mysql/my.cnf",
+  #       notify => Service["mysqld"],
+  #       require => Package["mysql-server"],
+  #   }
+
+  #  file { "/etc/my.cnf":
+  # require => File["/var/lib/mysql/my.cnf"],
+  #   ensure => "/var/lib/mysql/my.cnf",
+  #   }
+
+}
+
+
+define mysql::password ($password = "", $username = "root"){
+  include mysql
+
+  exec { "mysql::password::set-mysql-password ${password}":
+    unless => "mysqladmin -u${username} -p'${password}' status",
+    path => ["/bin", "/usr/bin"],
+    command => "mysqladmin -u${username} password '${password}'",
+    require => Class['mysql::service']
+  }
+}
+
+define mysql::create_database ( $username, $password, $root_password ){
+  include mysql
+
+  $create_sql = "create database ${name};"
+  $grant_sql = "grant all on ${name}.* to ${username}@localhost identified by '$password';"
+
+   exec { "create-${name}-db":
+        unless => "/usr/bin/mysql -uroot -p${root_password} ${name}",
+        command => "/usr/bin/mysql -uroot -p${root_password} -e \"${create_sql}\"",
+  require => Class['mysql::service']
+      }
+
+      exec { "grant-${name}-db":
+        unless => "/usr/bin/mysql -u${username} -p${password} ${name}",
+        command => "/usr/bin/mysql -uroot -p${root_password} -e \"${grant_sql}\"",
+        require => [Class["mysql::service"], Exec["create-${name}-db"]]
+      }
 }
