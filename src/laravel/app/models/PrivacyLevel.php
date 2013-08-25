f0752b6f7a5c9b89c3331da6e709a852bb5748d0
bchhun <bernard.chhun@gmail.com>
2013-08-25 14:52:55 -0400

    added a comment over the protected $table variable

--- src/laravel/app/models/PrivacyLevel.php
+++ src/laravel/app/models/PrivacyLevel.php
@@ -2,6 +2,11 @@
 
 class PrivacyLevel extends Eloquent {
 
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
     protected $table = "PrivacyLevel";
 	protected $guarded = array();
 
